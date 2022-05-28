432aef0058eee3b1ff31cf38aa63ef0c17bbe39b
olarag <oscar@difusion.mx>
2022-05-27 21:39:33 -0500

    se agregaron los metodos de la API

--- app.js
+++ app.js
@@ -9,3 +9,40 @@ app.get('/', (req,res) => {
 app.listen(port, () => {
 	console.log(`Example app listening on ${port}`)
 })
+
+app.get('/v1/explorers', (req,res) => {
+	console.log(`Àpi Explorers GET ALL request ${new Date()}`)
+	const explorer1 = {id: 1, name: "Explorer1"}
+	const explorer2 = {id: 2, name: "Explorer2"}
+	const explorer3 = {id: 3, name: "Explorer3"}
+	const explorer4 = {id: 4, name: "Explorer4"}
+	const explorers = [explorer1, explorer2, explorer3, explorer4]
+	res.status(200).json(explorers)
+})
+
+app.get('/v1/explorers/:id', (req, res) => {
+	console.log(`Api Explorers GET request ${new Date()}`)
+	console.log(`Getting explorer with id ${req.params.id}`)
+	const explorer = {id: 1, name: "Oscar"}
+	res.status(200).json(explorer)
+})
+
+app.post('/v1/explorers', (req, res) => {
+	console.log(`Api Explorers POST request ${new Date()}`)
+	const requestBody = req.body
+	res.status(201).json({mesagge: "Created"})
+})
+
+app.put('/v1/explorers/:id', (req, res) => {
+	console.log(`Api Explorers PUT request ${new Date()}`)
+	console.log(`Update explorer with id ${req.params.id}`)
+	const requestBody = req.body
+	res.status(200).json({message: "Updated!"})
+})
+
+app.delete('/v1/explorers/:id', (req, res) => {
+	console.log(`Api Explorers DELETE request ${new Date()}`)
+	console.log(`Delete explorer with id ${req.params.id}`)
+	const requestBody = req.body
+	res.status(200).json({message: "Deleted"})	
+})
